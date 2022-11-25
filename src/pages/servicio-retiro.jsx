5fdfc465b23064d9ef55546d983f52954d6c0c20
v1c70r-dev <sproutvmc@gmail.com>
2022-11-25 19:35:09 -0300

    Creacion servicio de retiro

--- src/pages/servicio-retiro.jsx
+++ src/pages/servicio-retiro.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {HomeLayout} from '../components/homeLayout';
-import {Typography } from '@mui/material';
+import { FormularioRetiro } from '../components/servicio-retiro/formularioRetiro';
+import { Typography,Grid } from '@mui/material';
 
 const ServicioRetiroPage = () => {
   return (
     <HomeLayout>
-      <Typography>Servicio Retiro</Typography>
+      <Grid item xs={12} sx={{display:'flex', justifyContent:'center', padding:'20px'}}>
+        <Typography variant='h3'> Formulario para servicio de retiro </Typography>
+      </Grid>
+      <FormularioRetiro/>
     </HomeLayout>
   )
 }
